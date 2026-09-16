8f17897fbd9e5a8888ba6e81c0136ae0a7337f26
hasithperera2012 <hasithsandul@gmail.com>
2026-09-16 09:20:17 -0400

    class 9/16/2026

--- app.py
+++ app.py
@@ -3,22 +3,65 @@ from turtle import *
 t = Turtle()
 
 t.shape('turtle')
-
+t.speed(100000000)
 # t.forward(400)
 
 # def message(input):
 #     print(input)
 # message("Hello World")
 
-t.shape('turtle')
-t.speed(10)
-t.forward(300)
-t.left(90)
-t.forward(300)
-t.left(90)
-t.forward(300)
-t.left(90)
-t.forward(300)
-t.left(90)
+# t.shape('turtle')
+# t.speed(10)
+# t.forward(300)
+# t.left(90)
+# t.forward(300)
+# t.left(90)
+# t.forward(300)
+# t.left(90)
+# t.forward(300)
+# t.left(90)
 
-turtle.done()
+# def equal(x):
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+# equal(400)
+
+# def equal(x):
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+# equal(200)
+
+# def right():
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(135)
+#     t.forward(142)
+# right()
+
+
+# for i in range(3):
+#     print(i)
+
+# for i in range(4):
+#     t.forward(100)
+#     t.left(90)
+
+for i in range(1000):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(95)
+
+turtle.done
