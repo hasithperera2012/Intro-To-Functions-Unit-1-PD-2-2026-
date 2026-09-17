1fb0e96b5907d02d2c8bd366c3014edf545db9d7
hasithperera2012 <hasithsandul@gmail.com>
2026-09-17 09:19:28 -0400

    class 9/17/2026

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from turtle import *
 t = Turtle()
 
 t.shape('turtle')
-t.speed(100000000)
+t.speed(10000)
 # t.forward(400)
 
 # def message(input):
@@ -54,14 +54,36 @@ t.speed(100000000)
 #     t.forward(100)
 #     t.left(90)
 
-for i in range(1000):
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(95)
+# for i in range(60):
+#     t.forward(200)
+#     t.left(91)
+#     t.forward(200)
+#     t.left(91)
+#     t.forward(200)
+#     t.left(91)
+#     t.forward(200)
+#     t.left(95)
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(100):
+        t.forward(x)
+        t.left(y)
+square(101,91)
+
+# def doubleSquares(iRange):
+#     length = 25
+#     for i in range(iRange):
+#         square(length, 90)
+#         length = length * 2
+# doubleSquares(100)
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+addSquares(5)
 
 turtle.done
